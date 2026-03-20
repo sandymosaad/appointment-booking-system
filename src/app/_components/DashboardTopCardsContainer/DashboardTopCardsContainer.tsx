14c79ceb086403ba3aad07061c476b85a8f2dcd8
Sandy Mosaad <sandyzakher5@gmail.com>
2026-03-20 16:40:46 +0200

    fix grid in top cards container

--- src/app/_components/DashboardTopCardsContainer/DashboardTopCardsContainer.tsx
+++ src/app/_components/DashboardTopCardsContainer/DashboardTopCardsContainer.tsx
@@ -1,15 +1,22 @@
-import { Grid} from "@mui/material";
+import { Grid } from "@mui/material"; 
 import DashboardCard from "../DashboardCard/DashboardCard";
 
-export default function DashboardTopCardsContainer({topCardsData,loading}) {
+export default function DashboardTopCardsContainer({ topCardsData, loading }) {
   return (
-    <Grid  spacing={2}  sx={{  display: "flex", flexWrap: "wrap", justifyContent: "space-between" , flexDirection: { xs: "column", md: "row" },  gap: 2 }}>
-        {topCardsData.map((card, index) => (
-            <Grid item xs={12} sm={6} md={4} key={index} sx={{  display: "flex",  justifyContent: "center" }}>
-            <DashboardCard card={card} loading={loading}  />
-            </Grid>
-        ))}
+    <Grid 
+      container 
+      spacing={2} 
+      sx={{ justifyContent: "space-between" }}
+    >
+      {topCardsData.map((card, index) => (
+        <Grid 
+          key={index} 
+          size={{ xs: 12, sm: 6, md: 4 }} 
+          sx={{ display: "flex", justifyContent: "center" }}
+        >
+          <DashboardCard card={card} loading={loading} />
+        </Grid>
+      ))}
     </Grid>
-  )
+  );
 }
-
